8ee72aae00a7f2b848540f98ce68f7873c3655a9
jeichman <joshua.eichman@nrel.gov>
2020-10-09 12:15:53 -0700

    Remove extraneous files

--- 225a/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\jeichman\Documents\gamsdir\projdir\RODeO\225a\" "C:\Users\jeichman\Documents\gamsdir\projdir\RODeO\" %1 "C:\Users\jeichman\Documents\gamsdir\projdir\RODeO\225a\gamscntr.dat" "C:\GAMS\win64\27.3\" "CPLEX"
--- 225f/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\jeichman\Documents\gamsdir\projdir\RODeO\225f\" "C:\Users\jeichman\Documents\gamsdir\projdir\RODeO\" %1 "C:\Users\jeichman\Documents\gamsdir\projdir\RODeO\225f\gamscntr.dat" "C:\GAMS\win64\27.3\" "CPLEX"
